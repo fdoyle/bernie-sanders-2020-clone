1943241c2711e19a03bccab7dffc542a19590ada
Frank Doyle <fdoyl001@gmail.com>
2020-02-06 15:46:06 -0600

    fix metadata and site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bernie Sanders - Official Campaign Website`,
+    description: `This website is for educational purposes only. Not in any way affiliated with the Bernie Sanders Campaign. `,
+    author: `Frank Doyle, copy belongs to the Bernie Sanders Campaign`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.webp`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
